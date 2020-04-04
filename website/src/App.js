399688929d5315abdd176ab42fcbc937ebf3b140
phwu123 <peterbhwu@gmail.com>
2020-04-03 23:51:24 -0700

    added react

---
+++ website/src/App.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import './App.css';
+import SiteHeader from './components/SiteHeader/SiteHeader';
+
+function App() {
+  return (
+    <div className="kinfolk">
+      <SiteHeader></SiteHeader>
+    </div>
+/*
+    <website>
+      <header>
+        kinfolk label
+        button section
+      </header>
+      <home>
+        photo gallery
+        follow us links
+        title
+        copyright
+      </home>
+      <about>
+        logo
+        about us
+      </about>
+      <relief>
+        title
+        button
+        mission
+        how can i apply
+        how can i help - donate
+      </relief>
+      <contact>
+        links
+        email
+      </contact>
+    
+    </website>
+
+*/
+  );
+}
+
+export default App;
---
+++ website/src/components/SiteHeader/SiteHeader.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './SiteHeader.css';
+
+function SiteHeader() {
+  return(
+    <div className="site-header">
+      header
+    </div>
+  )
+}
+
+export default SiteHeader
